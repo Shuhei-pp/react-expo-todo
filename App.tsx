524415e6c583bd62093cdc6d7d18fc54168ea30c
Shuhei-pp <lotakotayota@gmail.com>
2022-10-11 00:23:51 +0900

    アプリ作ってみた

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default function App() {
+  const [text, setText] = useState<string>("")
+  const [todos, setTodos] = useState<string[]>([])
+  const handleSubmit = () => {
+    const entityTodos = todos
+    entityTodos.push(text)
+    setTodos(entityTodos)
+    setText("")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1,width:200 }}
+        onChangeText={(event) => (setText(event))}
+        value={text}
+      />
+      <Button
+        title={"登録"}
+        onPress={()=>{handleSubmit()}}
+      />
+      {todos.map((todo,index) => {
+        return <Text key={ index}>{ index}:{todo}</Text>
+      })}
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +38,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginTop: '2%',
+      marginBottom: '2%'
+  }
 });
