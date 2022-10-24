392a80a9f9f947809aff5f5d7f8819ea557ef5dc
Shuhei-pp <lotakotayota@gmail.com>
2022-10-24 14:49:54 +0900

    backend環境構築

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { StatusBar } from 'expo-status-bar'
-import React,{ useState } from 'react'
-import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput } from 'react-native'
-import { Home } from './pages/Home'
-import { Todo } from './pages/Todo'
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import React from "react"
+import { Home } from "./pages/Home"
+import { Todo } from "./pages/Todo"
 
 export type RootStackParamList = {
   Home: undefined
@@ -16,11 +14,10 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Todo" component={Todo} />
       </Stack.Navigator>
     </NavigationContainer>
-
   )
 }
